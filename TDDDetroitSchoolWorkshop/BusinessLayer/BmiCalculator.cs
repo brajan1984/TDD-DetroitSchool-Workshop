bc917aad5a9afe891e3862441aecb8f9cb0bc14c
BRAJAN1984\bpiet <blazej.pietrzyk@gmail.com>
2018-07-28 20:36:03 +0200

    Step 2: Green

--- TDDDetroitSchoolWorkshop/BusinessLayer/BmiCalculator.cs
+++ TDDDetroitSchoolWorkshop/BusinessLayer/BmiCalculator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Linq;
 
 namespace TDDDetroitSchoolWorkshop.BussinessLayer
 {
@@ -6,7 +8,11 @@ namespace TDDDetroitSchoolWorkshop.BussinessLayer
     {
         public double CalculateBmiFromCsv(string row)
         {
-            throw new NotImplementedException();
+            var splittedValues = row.Split(";").ToList();
+            var heightInCm = double.Parse(splittedValues[0], CultureInfo.InvariantCulture);
+            var weightInKg = double.Parse(splittedValues[1], CultureInfo.InvariantCulture);
+
+            return Math.Round(weightInKg/Math.Pow((heightInCm/100), 2), 1);
         }
     }
 }
