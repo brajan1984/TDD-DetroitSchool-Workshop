54adbc2874b5486d795e37f48ab3394ed76caf3a
BRAJAN1984\bpiet <blazej.pietrzyk@gmail.com>
2018-07-28 20:44:10 +0200

    Step 3: Refactor

--- TDDDetroitSchoolWorkshop/BusinessLayer/BmiCalculator.cs
+++ TDDDetroitSchoolWorkshop/BusinessLayer/BmiCalculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -8,11 +9,25 @@ namespace TDDDetroitSchoolWorkshop.BussinessLayer
     {
         public double CalculateBmiFromCsv(string row)
         {
-            var splittedValues = row.Split(";").ToList();
-            var heightInCm = double.Parse(splittedValues[0], CultureInfo.InvariantCulture);
-            var weightInKg = double.Parse(splittedValues[1], CultureInfo.InvariantCulture);
+            var splittedValues = SplitValues(row);
+            var numbers = ParseNumbers(splittedValues);
+            var heightInCm = numbers[0];
+            var weightInKg = numbers[1];
 
             return Math.Round(weightInKg/Math.Pow((heightInCm/100), 2), 1);
+        }
+
+        private List<string> SplitValues(string row)
+        {
+            return row.Split(";").ToList();
+        }
+
+        private List<double> ParseNumbers(List<string> numbersToParse)
+        {
+            return new List<double> {
+                double.Parse(numbersToParse[0], CultureInfo.InvariantCulture),
+                double.Parse(numbersToParse[1], CultureInfo.InvariantCulture)
+            };
         }
     }
 }
